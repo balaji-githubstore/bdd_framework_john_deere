4c9a38a14b0741dd32ed012b5807e756c039c834
balaji-githubstore <31404824+balaji-githubstore@users.noreply.github.com>
2022-05-19 12:44:26 +0530

    added feature detail in runner file

--- src/test/java/com/johndeere/runner/RunnerTest.java
+++ src/test/java/com/johndeere/runner/RunnerTest.java
@@ -3,6 +3,9 @@ package com.johndeere.runner;
 import org.junit.runner.RunWith;
 
 import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+
+@CucumberOptions(features = {"src/test/resources/Feature/Login.feature"})
 
 @RunWith(Cucumber.class)
 public class RunnerTest {

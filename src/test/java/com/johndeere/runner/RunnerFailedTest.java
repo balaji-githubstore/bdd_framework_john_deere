52a34cfc879f820e71ab124a88c4e4b87ec13697
balaji-githubstore <31404824+balaji-githubstore@users.noreply.github.com>
2022-05-20 13:12:28 +0530

    rerun failed scenario

--- src/test/java/com/johndeere/runner/RunnerFailedTest.java
+++ src/test/java/com/johndeere/runner/RunnerFailedTest.java
@@ -4,18 +4,19 @@ import org.junit.runner.RunWith;
 
 import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
-
+//to rerun failed test scenario features = {"@target/rerun.txt"}
 @CucumberOptions(
-		features = {"src/test/resources/Feature"}
+		features = {"@target/rerun.txt"}
 		,glue = {"com.johndeere.stepdefn","com.johndeere.base"}
 		,publish = true
 		,monochrome = true
 		,plugin = {"rerun:target/rerun.txt","pretty","html:target/cucumber-report.html","json:target/cucumber-report.json"}
 		//,dryRun = true
-		,tags = "@invalid"
+//		,tags = "@valid or @patient"
+		,tags="@invalid"
 		)
 
 @RunWith(Cucumber.class)
-public class RunnerTest {
+public class RunnerFailedTest {
 
 }

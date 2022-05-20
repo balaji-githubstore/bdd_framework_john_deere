1cf0ef67733fb34db91a04b60c89bc862f22c19c
balaji-githubstore <31404824+balaji-githubstore@users.noreply.github.com>
2022-05-20 13:04:59 +0530

    embed screenshot to cucumber report

--- src/test/java/com/johndeere/base/AutomationHooks.java
+++ src/test/java/com/johndeere/base/AutomationHooks.java
@@ -1,13 +1,17 @@
 package com.johndeere.base;
 
 import java.time.Duration;
+import java.time.LocalDateTime;
 
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
 import io.cucumber.java.After;
+import io.cucumber.java.Scenario;
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class AutomationHooks {
@@ -32,8 +36,20 @@ public class AutomationHooks {
 	}
 
 	@After
-	public void end() {
+	public void end(Scenario scenario) {
+		System.out.println(scenario.getName());
+		System.out.println(scenario.isFailed());
+	
+		if(scenario.isFailed())
+		{
+			TakesScreenshot ts=(TakesScreenshot) driver;
+			byte[] bytes= ts.getScreenshotAs(OutputType.BYTES);
+			scenario.attach(bytes, "image/png", "SC_"+scenario.getName()+" "+LocalDateTime.now());
+		}
+		
 		driver.quit();
 	}
 
+	
+	
 }
